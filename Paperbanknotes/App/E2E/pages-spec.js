ced9ef7e34dc23e0055cf126608e5638ee6d8d73
ArjanR96 <a.roos@websend.nl>
2017-04-18 21:48:05 +0200

    init shit

---
+++ Paperbanknotes/App/E2E/pages-spec.js
@@ -0,0 +1,7 @@
+﻿describe('AutoTrader pages check', function () {
+    var scope, rootScope, ctrl, location;
+
+    it('should GET home', function () {
+        browser.get('http://localhost:5000/index.html#/');
+    });
+});
